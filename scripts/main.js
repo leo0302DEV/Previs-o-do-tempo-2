b830ecd6a6d9f6915c99378ebe69b3746dd7d7d1
Leonardo Kramer Nadal <leokn0102@gmail.com>
2023-12-24 17:26:12 -0300

    Iniciando css e html da página more-info.html com mobile first, designe ainda não responsivo, sendo ideal para telas de no máximo 400px, esta particulariedade será corrigida no próximo commit.

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/*
+Five days forecasts:
+http://dataservice.accuweather.com/forecasts/v1/daily/5day/${city code}&apikey=${}&language=${}&metric={true or false};
+
+City coder:
+http://dataservice.accuweather.com/locations/v1/cities/search&apikey=${}&q={input value}&language=${}&offset=1&alias=${};
+
+Current conditions:
+http://dataservice.accuweather.com/currentconditions/v1/${city code}&apikey={}&language=${};
+
+*/

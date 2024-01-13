c0a88fea468a30969e2fd21aca9777eb9d05b76a
Leonardo Kramer Nadal <leokn0102@gmail.com>
2024-01-12 21:10:58 -0300

    Correção de bug do tipo 'Mixed Content'

--- scripts/doRequest.js
+++ scripts/doRequest.js
@@ -3,7 +3,7 @@ const api_key = "	wh2D65icKGq0SFJUCLKTrkzwTodYmqsq";
 async function searchGeolocateInfo(search) {
   try {
     const response = await fetch(
-      `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${api_key}&q=${search}&offset=1`
+      `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${api_key}&q=${search}&offset=1`
     );
     const data = await response.json();
     return data;
@@ -17,7 +17,7 @@ async function searchWeatherData(response, lang, metric) {
     const cityCode = response[0].Key;
 
     const WeatherInfo = await fetch(
-      `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityCode}?apikey=${api_key}&language=${lang}&details=true&metric=${metric}`
+      `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityCode}?apikey=${api_key}&language=${lang}&details=true&metric=${metric}`
     );
     const weatherData = await WeatherInfo.json();
     return weatherData;
